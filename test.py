a76ba2ad6939b5e3348b7b11f07dfc466209a375
Amaryllis750 <danielchukwuemela751@gmail.com>
2024-06-16 15:54:49 +0100

    Update test.py
    We made the test.py into a functional program

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
 import random
+
+def guess_number():
+  randomNumber = random.randint(1, 100)
+  chances = 15
+  score = (chances / 15) * 100
+  
+  while chances > 0:
+    userinput = input("Guess the number: ")
+    if userinput == randomNumber:
+      print("Your guess is correct :)")
+    else:
+      if userinput < randomNumber:
+        print("Your guess is lower")
+      else:
+        print("Your guess is higher")
+    chances -= 1
+
+  print("Your score is {}".format(score))
+
+guess_number()
+  
